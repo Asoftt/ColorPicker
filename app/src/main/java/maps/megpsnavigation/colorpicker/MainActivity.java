869b3f904ba9bba46e1a47e607e2d7b6a5852663
Aqdas <aqdasmalik0335@gmail.com>
2019-07-16 09:55:40 +0500

    Initial commit

---
+++ app/src/main/java/maps/megpsnavigation/colorpicker/MainActivity.java
@@ -0,0 +1,58 @@
+package maps.megpsnavigation.colorpicker;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.larswerkman.holocolorpicker.ColorPicker;
+import com.larswerkman.holocolorpicker.OpacityBar;
+import com.larswerkman.holocolorpicker.SVBar;
+import com.larswerkman.holocolorpicker.SaturationBar;
+import com.larswerkman.holocolorpicker.ValueBar;
+
+public class MainActivity extends AppCompatActivity {
+int initialColor;
+    ConstraintLayout constraintLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        constraintLayout = findViewById(R.id.v);
+
+        final ColorPicker picker = (ColorPicker) findViewById(R.id.picker);
+        SVBar svBar = (SVBar) findViewById(R.id.svbar);
+        OpacityBar opacityBar = (OpacityBar) findViewById(R.id.opacitybar);
+        SaturationBar saturationBar = (SaturationBar) findViewById(R.id.saturationbar);
+        ValueBar valueBar = (ValueBar) findViewById(R.id.valuebar);
+
+        picker.addSVBar(svBar);
+        picker.addOpacityBar(opacityBar);
+        picker.addSaturationBar(saturationBar);
+        picker.addValueBar(valueBar);
+
+        picker.setOldCenterColor(picker.getColor());
+
+        picker.setOnColorChangedListener(new ColorPicker.OnColorChangedListener() {
+            @Override
+            public void onColorChanged(int color) {
+
+                constraintLayout.setBackgroundColor(color);
+            }
+        });
+        constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "touched", Toast.LENGTH_SHORT).show();
+            }
+        });
+      saturationBar.setOnSaturationChangedListener(new SaturationBar.OnSaturationChangedListener() {
+          @Override
+          public void onSaturationChanged(int saturation) {
+              constraintLayout.setBackgroundColor(saturation);
+          }
+      });
+
+    }
+}
